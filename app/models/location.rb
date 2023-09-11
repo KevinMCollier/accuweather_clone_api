831dee500737be211e4a69bd941abd22bcef5eea
kevin collier <collier.kevin.m@gmail.com>
2023-09-11 12:51:19 +0900

    added validations to models

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,4 +1,8 @@
 class Location < ApplicationRecord
   belongs_to :user
   has_many :forecasts
+
+  validates :name, presence: true
+  validates :latitude, presence: true
+  validates :longitude, presence: true
 end
