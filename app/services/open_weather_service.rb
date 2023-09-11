a11ecd6a9bc78c94396b0720f3781d5fd9b61bbc
kevin collier <collier.kevin.m@gmail.com>
2023-09-11 18:05:45 +0900

    created service for open weather fetch

---
+++ app/services/open_weather_service.rb
@@ -0,0 +1,21 @@
+require 'open-uri'
+require 'json'
+
+class OpenWeatherService
+  BASE_URL = 'http://api.openweathermap.org/data/2.5'
+
+  def initialize(city)
+    @city = city
+  end
+
+  def fetch_weather
+    response = open("#{BASE_URL}/weather?q=#{@city}&appid=#{ENV['OPEN_WEATHER_MAP_API_KEY']}&units=metric").read
+    JSON.parse(response)
+  rescue OpenURI::HTTPError => e
+  end
+
+  def fetch_forecast
+    response = open("#{BASE_URL}/forecast?q=#{@city}&appid=#{ENV['OPEN_WEATHER_MAP_API_KEY']}&units=metric").read
+    JSON.parse(response)
+  end
+end
