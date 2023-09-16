e0c9e00d66dbdd511ee66a68a3322e42a771ce41
kevin collier <collier.kevin.m@gmail.com>
2023-09-16 22:05:58 +0900

    changed to 3001

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3000' # adjust this if your React app is on a different port
+    origins 'localhost:3001' # adjust this if your React app is on a different port
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :delete, :options]
