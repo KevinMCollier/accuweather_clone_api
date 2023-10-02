5df7715c15531516ab51d9a331366739d1e33ddb
kevin collier <collier.kevin.m@gmail.com>
2023-10-02 20:49:55 +0900

    updated cors for heroku

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'localhost:3001' # adjust this if your React app is on a different port
+    origins ['localhost:3001', 'wonderful-salmiakki-9d2149.netlify.app']
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :delete, :options]
